7b7040bbdb744d95d5b19cb258b0396f7a4a3ad5
Moh. Sirajul Umam <sirajul.umam8@gmail.com>
2024-06-06 13:18:05 +0700

    Menambah Todolist

--- src/AplikasiTodolist.java
+++ src/AplikasiTodolist.java
@@ -29,8 +29,33 @@ public class AplikasiTodolist {
     /**
      * Menambahkan todo list
      */
-    public static void addTodolist() {
+    public static void addTodolist(String todo) {
+        var isFull = true;
 
+        // cek apakah model penuh
+        for (int i = 0; i < model.length; i++) {
+            if (model[i] == null) {
+                isFull = false;
+                break;
+            }
+        }
+
+        // jika penuh resize ukuran array 2x lipat
+        if (isFull) {
+            var tmp = model;
+            model = new String[model.length * 2];
+            for (int i = 0; i < tmp.length; i++) {
+                model[i] = tmp[i];
+            }
+        }
+
+        // tambahkah ke posisis data array-nya null
+        for (int i = 0; i < model.length; i++) {
+            if (model[i] == null) {
+                model[i] = todo;
+                break;
+            }
+        }
     }
 
     /**
